2e2cef9e6c77f68b82a9215844ceb246680bb758
Holly-Ellington <146004914+Holly-Ellington@users.noreply.github.com>
2024-01-19 14:00:12 -0600

    Update posts.router.js to improve readability

--- src/posts/posts.router.js
+++ src/posts/posts.router.js
@@ -2,7 +2,9 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./posts.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").post(controller.create).all(methodNotAllowed);
+router.route("/")
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 router
   .route("/:postId")
